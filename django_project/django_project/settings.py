21b106f1b6a56cfebc3a4acfe9566e06d6ee1108
Zacson191424204 <zacson191424@gmail.com>
2025-03-11 12:49:01 -0600

    Estructura del proyecto Django 📂

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Define la ruta donde se recopilarán los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+staticfiles_dirs = [
+    os.path.join(BASE_DIR, 'blog/static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -128,3 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 staticfiles_dirs = [
     os.path.join(BASE_DIR, 'blog/static')
 ]
+
+STATIC_URL = 'static/'
+
+# Define la ruta donde se recopilarán los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+staticfiles_dirs = [
+    os.path.join(BASE_DIR, 'blog/static')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
