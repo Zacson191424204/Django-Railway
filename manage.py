9da9a1f3740972b9284b134dfc887086c590d7c6
Zacson191424204 <zacson191424@gmail.com>
2025-03-12 19:17:25 -0600

    correcion de estructura del proyecto django

--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
-# filepath: C:\Users\PC GAMING\Desktop\Django-Railway\manage.py
 import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.django_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
